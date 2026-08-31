22ccaee3d7d273be6cf32eabc68ea0554225d0fb
Anderson Fabian Garcia Nieto <anderson.fgn13@gmail.com>
2026-08-30 21:13:37 -0500

    chore: scaffold NestJS project with env validation and health check

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { AppController } from './app.controller';
+import { envValidationSchema } from './config/env.validation';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envValidationSchema,
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: Number(process.env.THROTTLE_TTL ?? 60000),
+        limit: Number(process.env.THROTTLE_LIMIT ?? 20),
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
